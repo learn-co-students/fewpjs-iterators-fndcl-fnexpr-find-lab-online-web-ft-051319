0135afc7f8a61319b9df02e31cead7fc033f389f
Travis Exline <t.exline22@gmail.com>
2019-08-26 17:20:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(function(game) {return game.result === "W"})
+  return win ? win.year : win
+}
