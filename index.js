31afe802d0da6db1171c9a4d6de1acc88610fbff
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-26 14:45:31 +0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(array){
+  let object = array.find(function(e) {return e.result==="W"})
+  return object ? object.year : object
 }
