069603a9341fa2523c52a1dd067c9d3e9e812505
Brian <briandsalemi@gmail.com>
2019-08-27 10:59:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const testVar = {}
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
 function testFunc() {
   return "hi"
+}
+
+function superbowlWin(record) {
+  record.find( function(record) {
+    console.log(record);
+    if(record.result === "W") {
+      return record.year
+    } else { 
+      return undefined 
+    } 
+  })
 }
