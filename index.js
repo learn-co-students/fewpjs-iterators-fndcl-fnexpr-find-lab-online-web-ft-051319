429f71b3b31501010c627575c549accfaabaa39e
mkonig411blk <margot.konig@blackrock.com>
2019-08-26 22:17:05 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+    let newArray = array.find(s => s.result === "W");
+    return newArray === undefined ? newArray : newArray.year;
+}
