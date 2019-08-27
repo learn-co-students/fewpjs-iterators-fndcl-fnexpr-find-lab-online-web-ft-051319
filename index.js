58b547498761badd8a1001f8e42a5eb90670167c
jenniferbusca <jennifer.busca@gmail.com>
2019-08-27 18:01:38 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+// function superbowlWin(array) {
+//   array.find(function(e) {
+//     debugger
+//     if(e.result === "W") {
+//       return e.year
+//     }
+//   })
+// }
+function superbowlWin(array) {
+  let winner =  array.find(function(e) {return e.result === "W" })
+  return winner === undefined ? undefined : winner.year
+}
+
+// function superbowlWin(array) {
+//   let winner =  array.find(function(e) {return e.result === "W" })
+//   if (winner === undefined){
+//     return undefined
+//   }
+//   else {
+//     return winner.year
+//   }
+// }
