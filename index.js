b0618d70ebcf7aa2319f514644d6032e8fb7d994
Irma Mejia <ijanettmejia@outlook.com>
2019-09-05 22:28:05 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find(function(e) {return e.result === "W"})
+  return win === undefined ? undefined : win.year
+}
