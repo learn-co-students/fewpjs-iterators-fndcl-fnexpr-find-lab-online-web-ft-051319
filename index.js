dc2a248e0244fee3f354ec540db13f3ba984403e
Kane Koo <kanekoo@gmail.com>
2019-09-09 01:02:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const testVar = {}
+const testVar = [
+  { year: "2018", result: "N/A"},
+  { year: "2017", result: "N/A"},
+  { year: "2016", result: "N/A"}
+]
 
 function testFunc() {
   return "hi"
+}
+function superbowlWin (record) {
+  let row = record.find(function(row) { return row["result"]==="W"})
+ if (row === undefined)
+  return undefined
+  else {
+    return row["year"]
+  }
+
 }
