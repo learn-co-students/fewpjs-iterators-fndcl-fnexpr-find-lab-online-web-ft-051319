f7cf9b19ac85710b3859fcfdf4a6c135c30742a3
jenniferbusca <jennifer.busca@gmail.com>
2019-12-16 08:50:39 -0500

    Done.

--- index.js
+++ index.js
@@ -4,26 +4,7 @@ function testFunc() {
   return "hi"
 }
 
-
-// function superbowlWin(array) {
-//   array.find(function(e) {
-//     debugger
-//     if(e.result === "W") {
-//       return e.year
-//     }
-//   })
-// }
-function superbowlWin(array) {
-  let winner =  array.find(function(e) {return e.result === "W" })
-  return winner === undefined ? undefined : winner.year
+superbowlWin = (array) => {
+  let result = array.find(array => array.result === "W")
+  return result ? result.year : undefined
 }
-
-// function superbowlWin(array) {
-//   let winner =  array.find(function(e) {return e.result === "W" })
-//   if (winner === undefined){
-//     return undefined
-//   }
-//   else {
-//     return winner.year
-//   }
-// }
